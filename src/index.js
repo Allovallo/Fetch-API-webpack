9ba63ff19d6f412d545310f4df2580eadd60a63c
Allovallo <30566134+Allovallo@users.noreply.github.com>
2023-12-27 15:19:43 +0200

    Files updated

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import name from './module-1';
+import './css/common.css';
 
 console.log(name);
 
-console.log('Це index.js');
+console.log('Це дійсно index.js');
--- webpack.config.js
+++ webpack.config.js
@@ -7,4 +7,17 @@ module.exports = {
     path: path.resolve(__dirname, 'build'),
     filename: 'my-bundle.js',
   },
+  module: {
+    rules: [
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] },
+      { test: /\.ts$/, use: 'ts-loader' },
+    ],
+  },
+  devServer: {
+    open: true,
+    historyApiFallback: true,
+    static: './',
+    compress: true,
+    port: 8080,
+  },
 };
